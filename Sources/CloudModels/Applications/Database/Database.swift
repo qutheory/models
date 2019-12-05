be0eac9f9fc8b677ee5f3020c9747d35f9d26e84
Jonas Schwartz <jonas@josc.io>
2019-12-05 02:35:07 +0100

    Adding credentials

--- Sources/CloudModels/Applications/Database/Database.swift
+++ Sources/CloudModels/Applications/Database/Database.swift
@@ -3,16 +3,22 @@ public final class Database: Identifiable, Extensible {
     public var databaseServer: ModelOrIdentifier<DatabaseServer>
     public var environment: ModelOrIdentifier<Environment>
     public var extend: [String : Any]
+    public var username: String?
+    public var password: String?
     
     public init(
         id: Identifier? = nil,
         databaseServer: ModelOrIdentifier<DatabaseServer>,
-        environment: ModelOrIdentifier<Environment>
+        environment: ModelOrIdentifier<Environment>,
+	username: String? = nil,
+	password: String? = nil
     ) throws {
         self.id = id
         self.databaseServer = databaseServer
         self.environment = environment
         self.extend = [:]
+	self.username = username
+	self.password = password
     }
 }
 
@@ -24,13 +30,17 @@ extension Database: JSONConvertible {
         try self.init(
             id: json.get("id"),
             databaseServer: .identifier(json.get("databaseServer.id")),
-            environment: .identifier(json.get("application.id"))
+            environment: .identifier(json.get("application.id")),
+	    username: json.get("username"),
+	    password: json.get("password")
         )
     }
     
     public func makeJSON() throws -> JSON {
         var json = JSON()
         try json.set("id", id)
+	try json.set("username", username)
+	try json.set("password", password)
         try json.set("databaseServer", databaseServer.makeJSON())
         try json.set("environment", environment.makeJSON())
         return json
